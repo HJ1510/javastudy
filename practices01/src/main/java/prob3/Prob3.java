44b31eae4eb92e0ebccaa176d10a6c7ec0321d35
HJ1510 <yhj221004@gmail.com>
2022-10-21 16:22:45 +0900

    문제풀이중..

--- practices01/src/main/java/prob3/Prob3.java
+++ practices01/src/main/java/prob3/Prob3.java
@@ -7,9 +7,12 @@ public class Prob3 {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
+		
+		while(true) {
+			
+		}
 
 		/* 코드 작성 */
 		
-		scanner.close();
 	}
 }
