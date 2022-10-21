f2ed32d500de07077c1077793e56cff1ca45b111
HJ1510 <yhj221004@gmail.com>
2022-10-21 16:33:15 +0900

    문제풀이중..

--- practices01/src/main/java/prob3/Prob3.java
+++ practices01/src/main/java/prob3/Prob3.java
@@ -5,14 +5,22 @@ import java.util.Scanner;
 public class Prob3 {
 	
 	public static void main(String[] args) {
-
+		System.out.print("숫자를 입력하세요: ");
 		Scanner scanner = new Scanner(System.in);
-		
+		int num = scanner.nextInt();
+		int sum = 0;
+				
 		while(true) {
+			if(num % 2 == 1) { //홀수일때
+				sum=sum+num;
+				break;
+			} else if(num % 2 == 0) { //짝수일때
+				sum=sum+num;
+				System.out.println(sum);
+				break;
+			}
 			
-		}
-
-		/* 코드 작성 */
-		
+			
+		}	
 	}
 }
