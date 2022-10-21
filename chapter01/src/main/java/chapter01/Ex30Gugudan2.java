1be2dd4af872dc55b4a09169077b7b4615903a83
HJ1510 <yhj221004@gmail.com>
2022-10-21 15:39:04 +0900

    구구단

--- chapter01/src/main/java/chapter01/Ex30Gugudan2.java
+++ chapter01/src/main/java/chapter01/Ex30Gugudan2.java
@@ -5,7 +5,7 @@ public class Ex30Gugudan2 {
 	public static void main(String[] args) {
 		//System.out.println("1 X 1 = 1");
 		//System.out.println("1 X 1 = 1");
-//		
+		
 //		System.out.print("1 X 1 = 1");
 //		System.out.print("\n");
 //		System.out.print("1 X 1 = 1");
