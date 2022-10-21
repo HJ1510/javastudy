17c4d30fbc7b3feb22163469e3ee3fbfba576add
HJ1510 <yhj221004@gmail.com>
2022-10-21 16:21:31 +0900

    소수판별

--- chapter01/src/main/java/chapter01/Ex29PrimeDetect.java
+++ chapter01/src/main/java/chapter01/Ex29PrimeDetect.java
@@ -4,12 +4,19 @@ public class Ex29PrimeDetect {
 
 	public static void main(String[] args) {
 		// 2 ~ 1000 까지에서 소수를 화면에 출력 하세요.
-		
-		int num = 2;
-		boolean isPrime = true;
-		
+	
 		for(int i = 2; i <= 1000; i++) {
-			if (num%i==0) {
+			
+			boolean isPrime = true;
+			
+			for(int j=2; j<i; j++) {
+				if(i%j==0) {
+					isPrime = false;
+					break;
+				}
+			}
+			if(isPrime) {
+				System.out.println(i);
 			}
 		}
 	}		
