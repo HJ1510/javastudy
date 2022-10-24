a9a6229d0b2c7981b1d742a5a39221b1c9cede63
HJ1510 <yhj221004@gmail.com>
2022-10-24 16:37:06 +0900

    char[]공백변환

--- practice02/src/main/java/prob03/Prob03.java
+++ practice02/src/main/java/prob03/Prob03.java
@@ -2,16 +2,26 @@ package prob03;
 
 public class Prob03 {
 	public static void main(String args[]) {
-		char c[] = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l', '.' };
+		char[] charArray = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l', '.' };
 
 		// 원래 배열 원소 출력
-		printCharArray(c);
+		printCharArray(charArray);
 
 		// 공백 문자 바꾸기
-		replaceSpace(c);
-
+		for(int i =0; i<charArray.length; i++) {
+			if(charArray[i] == ' ') {
+				charArray[i] = ',';
+			}
+//			System.out.println(charArray[i]);
+				
+		}
+		
 		// 수정된 배열 원소 출력
-		printCharArray(c);
+		printCharArray(charArray);			
+	}		
+			
+	public static void printCharArray(char[] cs) {
+		System.out.println(cs);
+		
 	}
-
 }
