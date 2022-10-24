ede216c256c4526468c8bf8a34ad3c053df0df6a
HJ1510 <yhj221004@gmail.com>
2022-10-24 10:22:33 +0900

    1->19

--- practices01/src/main/java/prob2/Prob2.java
+++ practices01/src/main/java/prob2/Prob2.java
@@ -3,11 +3,11 @@ package prob2;
 public class Prob2 {
 	public static void main(String[] args) {
 		for(int i = 1; i <= 10; i++) {
-			for (int j = i; j <= 10; j++) {
-				System.out.print(j+"-");
+			System.out.print(i+" ");
+			for (int j = i+1 ; j <= i+9; j++) {
+				System.out.print(j+" ");
 			}
-			System.out.println("*");
-//			System.out.println(i+1+"*");
+			System.out.println();
 			
 		}
 	}
