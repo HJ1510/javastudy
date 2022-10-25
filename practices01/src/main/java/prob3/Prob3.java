84861cbad47bef03036f1c92077fcc020bb83d88
HJ1510 <yhj221004@gmail.com>
2022-10-25 13:00:50 +0900

    입력값까지 짝홀 합

--- practices01/src/main/java/prob3/Prob3.java
+++ practices01/src/main/java/prob3/Prob3.java
@@ -3,31 +3,29 @@ package prob3;
 import java.util.Scanner;
 
 public class Prob3 {
-	
+
 	public static void main(String[] args) {
 		System.out.print("숫자를 입력하세요: ");
 		Scanner scanner = new Scanner(System.in);
 		int num = scanner.nextInt();
 		int sum = 0;
-				
-		
-		for(int i = 0; i < num; i++) {
-			if(i % 2 == 1) { // 홀수일때
-				sum=sum+i;				
-				
-			} System.out.println(sum);
-				
-				
-////			if(num % 2 == 1) { //홀수일때
-//////				sum=sum+num;
-//////				System.out.println(sum);
-//				break;
-//			} else if(num % 2 == 0) { //짝수일때
-//				sum=sum+num;
-////				System.out.println(sum);
-//				break;
-			
-			
+		int sum2 = 0;
+
+		if (num % 2 == 0) { // num 짝수일때
+			for (int i = 0; i <= num; i++) {
+				if (i % 2 == 0) {
+					sum = sum + i;
+				}
+			}
+		} else { // num 홀수일때
+			for (int i = 0; i <= num; i++) {
+				if (i % 2 != 0) {
+					sum = sum + i;
+				}
+			}
+
 		}
+		System.out.println(sum);
+
 	}
 }
