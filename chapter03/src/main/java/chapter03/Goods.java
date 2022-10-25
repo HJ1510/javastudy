fc81f82794e878bd1d774e71b789733ce80cb5f3
HJ1510 <yhj221004@gmail.com>
2022-10-25 14:57:25 +0900

    객체지향

--- chapter03/src/main/java/chapter03/Goods.java
+++ chapter03/src/main/java/chapter03/Goods.java
@@ -1,12 +1,20 @@
 package chapter03;
+
 //정보은닉
 public class Goods {
+	public static int countOfGoods = 0; 
+	
 	private String name;
 	private int price;
 	private int countStock;
 	private int countSold;
 	
-		
+	
+	
+	public Goods() {
+		countOfGoods++;  // Goods.countOfGoods++;=> 같은 클래스 안에서 Goods.생략가능
+	}
+
 	public String getName() {
 		return name;
 	}
@@ -15,16 +23,17 @@ public class Goods {
 		this.name = name;
 	}
 
-	
 	public int getPrice() {
 		return price;
 	}
 
 	public void setPrice(int price) {
+		if (price < 0) {
+			price = 0;
+		}
 		this.price = price;
 	}
 
-	
 	public int getCountStock() {
 		return countStock;
 	}
@@ -32,7 +41,6 @@ public class Goods {
 	public void setCountStock(int countStock) {
 		this.countStock = countStock;
 	}
-
 
 	public int getCountSold() {
 		return countSold;
@@ -42,19 +50,16 @@ public class Goods {
 		this.countSold = countSold;
 	}
 
-
-
-
-
 	public String toString() {
-		return "Goods [name=" + name + ", price=" + price + 
-				", countStock=" + countStock + ", countSold=" + countSold + "]";
+		return "Goods [name=" + name + ", price=" + price + ", countStock=" + countStock + ", countSold=" + countSold
+				+ "]";
 	}
 	
+
 //	public void setName(String name) {
 //	this.name = name; //this는 runtime때 실행중인 객체를 내부에서 접근할때 필요
 //  }
-	
+
 //	public String getName() {
 //		return name;
 //	}
--- chapter03/src/main/java/chapter03/GoodsApp.java
+++ chapter03/src/main/java/chapter03/GoodsApp.java
@@ -3,15 +3,22 @@ package chapter03;
 public class GoodsApp {
 
 	public static void main(String[] args) {
-		Goods camera = new Goods();  //camera=>변수
+		Goods camera = new Goods(); // camera=>변수 new Goods()=>기본생성자
 		camera.setName("nikon");
-		camera.setPrice(400000);
+		camera.setPrice(-1);
 		camera.setCountStock(30);
 		camera.setCountSold(50);
-		
-//		System.out.println(camera);
+
 		System.out.println(camera.getName());
 		System.out.println(camera);
+
+		Goods g1 = new Goods();
+		Goods g2 = new Goods();
+		Goods g3 = new Goods();
+		
+		System.out.println(Goods.countOfGoods);
+		
+		System.out.println(g1);
 	}
 
 }
