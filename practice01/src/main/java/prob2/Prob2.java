10f77bb074b3350c7eeefb6c3bca764be3ff4c73
user <user@HOME>
2022-10-26 23:10:33 +0900

    1-19

---
+++ practice01/src/main/java/prob2/Prob2.java
@@ -0,0 +1,14 @@
+package prob2;
+
+public class Prob2 {
+	public static void main(String[] args) {
+		for(int i = 1; i <= 10; i++) {
+			System.out.print(i+" ");
+			for (int j = i+1 ; j <= i+9; j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+			
+		}
+	}
+}
