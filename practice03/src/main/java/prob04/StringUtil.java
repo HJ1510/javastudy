6476c7487faab3cac6b30544d5f9ce30513a1cd3
HJ1510 <yhj221004@gmail.com>
2022-10-27 16:45:02 +0900

    문자열배열 하나의 문자열로

--- practice03/src/main/java/prob04/StringUtil.java
+++ practice03/src/main/java/prob04/StringUtil.java
@@ -5,7 +5,9 @@ import java.util.Arrays;
 public class StringUtil {
 
 	public static String concatenate(String[] strArr) {
-		String result=strArr[0]+strArr[1]+strArr[2];
+		String result=Arrays.toString(strArr);
+		result=strArr[0]+strArr[1]+strArr[2];
+		
 		return result;
 	}
 }
