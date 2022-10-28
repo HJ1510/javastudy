47a666b618759bcc04432e3f41f94bb223337fb9
HJ1510 <yhj221004@gmail.com>
2022-10-28 16:12:19 +0900

    Depart 클래스 상속

--- practice04/src/main/java/prob04/Prob04.java
+++ practice04/src/main/java/prob04/Prob04.java
@@ -3,7 +3,7 @@ package prob04;
 public class Prob04 {
 
 	public static void main(String[] args) {
-		Employee pt = new Depart( "홍길동", 3000, "개발부" ); //colorpoint 비슷 employee 상속받아야
+		Employee pt = new Depart( "홍길동", 3000, "개발부" ); 
 		pt.getInformation();
 	}
 }
