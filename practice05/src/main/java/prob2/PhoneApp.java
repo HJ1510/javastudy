a710ff18c5769e3152744472524ddffcfc5e4a16
HJ1510 <yhj221004@gmail.com>
2022-10-28 15:04:19 +0900

    Practice05

--- practice05/src/main/java/prob2/PhoneApp.java
+++ practice05/src/main/java/prob2/PhoneApp.java
@@ -3,10 +3,10 @@ package prob2;
 public class PhoneApp {
 
 	public static void main(String[] args) {
-		Phone phone = new SmartPhone();
+		Phone phone = new MusicPhone();
 
 		phone.execute("음악");
 		phone.execute("통화");
-		phone.execute("앱");
+//		phone.execute("앱");
 	}
 }
--- practice05/src/main/java/prob4/SoundTest.java
+++ practice05/src/main/java/prob4/SoundTest.java
@@ -1,9 +1,9 @@
 package prob4;
 
-public class SoundTest {
+public class SoundTest { // paint 프로그램 비슷 인터페이스 오버라이드&임플리먼트
 
 	public static void main(String[] args) {
-		printSound( new Cat() );
+		printSound( new Cat() ); // new circle..
 		printSound( new Dog() );
 		printSound( new Sparrow() );
 		printSound( new Duck() );
