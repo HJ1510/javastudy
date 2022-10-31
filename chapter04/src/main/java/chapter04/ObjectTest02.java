555a0cf221de469158f89ea70bba02b50a20bcf0
HJ1510 <yhj221004@gmail.com>
2022-10-31 10:50:59 +0900

    API

--- chapter04/src/main/java/chapter04/ObjectTest02.java
+++ chapter04/src/main/java/chapter04/ObjectTest02.java
@@ -11,11 +11,19 @@ public class ObjectTest02 {
 		System.out.println(p1 == p2);
 		System.out.println(p2 == p3);
 
-		// equals : 두 객체의 동질성. 변수 안에 저장된 객체의 내용을 비교
+		// equals : 두 객체의 동질성. 객체의 내용을 비교
 		// 부모클래스 Object의 기본 구현은 동일성(==) 검토 .. 동질성을 검토하려면 오버라이딩 필요
 		
 		System.out.println(p1.equals(p2));
 		System.out.println(p2.equals(p3));
+		
+		
+		
+		
+		
+		
+		
+		
 		System.out.println("-------");
 
 		String s1 = "1";
