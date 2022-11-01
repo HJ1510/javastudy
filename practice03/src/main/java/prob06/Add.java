86aedfd48ceede0dc04e85cbd1c3880194219413
HJ1510 <yhj221004@gmail.com>
2022-11-01 09:11:13 +0900

    푸는중

--- practice03/src/main/java/prob06/Add.java
+++ practice03/src/main/java/prob06/Add.java
@@ -1,15 +1,10 @@
 package prob06;
 
-public class Add {
-	private int a;
-	private int b;
-	
-	public void setValue(int a, int b) {
-		this.a = a;
-		this.b = b;
-	}
-	
+public class Add extends Arith {
+
+	@Override
 	public int calculate() {
 		return a + b;
 	}
+
 }
---
+++ practice03/src/main/java/prob06/Arith.java
@@ -0,0 +1,31 @@
+package prob06;
+
+public abstract class Arith {
+
+	protected int a;
+	protected int b;
+
+	public void setValue(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+
+	public abstract int calculate();
+
+}
