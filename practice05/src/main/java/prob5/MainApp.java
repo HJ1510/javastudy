6e442f75e2c1de69be38e6c4b47861d895115af6
HJ1510 <yhj221004@gmail.com>
2022-11-02 14:24:32 +0900

    ..

--- practice05/src/main/java/prob5/MainApp.java
+++ practice05/src/main/java/prob5/MainApp.java
@@ -3,7 +3,7 @@ package prob5;
 public class MainApp {
 
 	public static void main(String[] args) {
-		try {
+//		try {
 			MyStack stack = new MyStack(3);
 			stack.push("Hello");
 			stack.push("World");
@@ -11,22 +11,23 @@ public class MainApp {
 			stack.push("java");
 			stack.push(".");
 
-			while (stack.isEmpty() == false) {
-				String s = stack.pop();
-				System.out.println( s );
-			}
+//			while (stack.isEmpty() == false) {
+//				String s = stack.pop();
+//				System.out.println( s );
+//			}
 
 			System.out.println("======================================");
-
-			stack = new MyStack(3);
-			stack.push("Hello");
-
-			System.out.println(stack.pop());
-			System.out.println(stack.pop());
+//
+//			stack = new MyStack(3);
+//			stack.push("Hello");
+//
+//			System.out.println(stack.pop());
+//			System.out.println(stack.pop());
 			
-		} catch ( MyStackException ex) {
-			System.out.println( ex );
-		}
+//		} 
+//		catch ( MyStackException ex) {
+//			System.out.println( ex );
+//		}
 
 	}
 
