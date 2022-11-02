e5b9e21948ea0618efa86f99483278d434cc9f95
HJ1510 <yhj221004@gmail.com>
2022-11-02 17:14:34 +0900

    IO

--- chapter04/src/main/java/io/PhoneList02.java
+++ chapter04/src/main/java/io/PhoneList02.java
@@ -1,19 +1,15 @@
 package io;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.FileSystemNotFoundException;
+import java.io.FileNotFoundException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
 public class PhoneList02 {
 
-	public static void main(String[] args) throws FileSystemNotFoundException {
-		BufferedReader br = null;
-		try {
-			// File I/O 상관없음
+	public static void main(String[] args) throws FileNotFoundException {
+		
 			File file = new File("phone.txt");
 			if (!file.exists()) {
 				System.out.println("file not found");
@@ -38,17 +34,6 @@ public class PhoneList02 {
 				System.out.println(name + ":" + phone01 + "-" + phone02 + "-" + phone03);
 			}
 			scanner.close();
-		} catch (IOException ex) {
-			System.out.println("Error:" + ex);
-
-		} finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+		
 	}
 }
