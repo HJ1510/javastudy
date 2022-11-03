e5b8545736f1f810c7884e81d0f4a55d589a18c4
HJ1510 <yhj221004@gmail.com>
2022-11-03 10:59:58 +0900

    Generic

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -1,26 +1,54 @@
+//package prob01; // 오버로드
+//
+//public class Printer {
+//	public Printer() {
+//	}
+//
+//	public void println(int i) {
+//		System.out.println(i);
+//	}
+//
+//	public void println(boolean b) {
+//		System.out.println(b);
+//	}
+//
+//	public void println(double d) {
+//		System.out.println(d);
+//	}
+//
+//	public void println(String s) {
+//		System.out.println(s);
+//	}
+//
+//}
+
 package prob01;
 
 public class Printer {
-	public Printer() {
 
+//	public <T> void println(T t) {
+//		System.out.println(t); // 변수타입을 호출할때(런타임때) 결정한다 동적타이핑
+//	}
+
+	
+	public <T> void println(T... ts) {
+		for(T t : ts) {
+			System.out.println(t);
+		}
 	}
 
-	public void println(int i) {
-		System.out.println(i);
-
+	public int sum(int... nums) { // ... 가변파라미터
+		int sum = 0;
+		for (int i : nums) {
+			sum = sum + i;
+		}
+		return sum;
 	}
 
-	public void println(boolean b) {
-		System.out.println(b);
-	}
-
-	public void println(double d) {
-		System.out.println(d);
-	}
-
-	public void println(String s) {
-		System.out.println(s);
-
-	}
+//	public <T, R, S>=>리턴 타입 아님 제너릭임을 표시해주는것 R println(T t, S a) {
+//		R result=null;
+//		return result;
+//		
+//	}
 
 }
--- practice04/src/main/java/prob01/PrinterTest.java
+++ practice04/src/main/java/prob01/PrinterTest.java
@@ -5,9 +5,20 @@ public class PrinterTest { // 오버로딩
 	public static void main(String[] args) {
 		Printer printer = new Printer();
 
-		printer.println( 10 );
-		printer.println( true );
-		printer.println( 5.7 );
-		printer.println( "홍길동" );
+//		printer.println(10);
+//		printer.println(true);
+//		printer.println(5.7);
+//		printer.println("홍길동");
+//		printer.println(new Point(10, 20));
+
+		printer.println("홍길동");
+		printer.println(10, true);
+		printer.println(10, true, 5.7, new Point(10, 20));
+
+		System.out.println(printer.sum(1));
+		System.out.println(printer.sum(1, 2));
+		System.out.println(printer.sum(1, 2, 3, 4)); // 가변파라미터
+//		System.out.println(printer.sum(new int[] { 1, 2, 3, 4 })); // 배열
+		System.out.println(printer.sum(1, 2, 3, 4, 5, 6));
 	}
 }
